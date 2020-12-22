234cf00e749a7a47e889c871b0ddb7f0d62299ef
N4r35h <r.nar12345@gmail.com>
2020-12-22 14:35:47 +0530

    Update iir.m

--- iir.m
+++ iir.m
@@ -4,8 +4,8 @@ close all;
 wp=[0.1 0.8]; % butter worth BSF [array of 2 points for bandpass and stop single point for low and high]
 ws=[0.2 0.7];
 rp=1; rs=50;
-[n,wn]=buttord(wp,ws,rp,rs);
-[b,a]=butter(n,wn,'stop'); %stop,bandpass,low,high
+[n,wn]=buttord(wp,ws,rp,rs); %cheb1ord,buttord
+[b,a]=butter(n,wn,'stop'); %cheby1,butter %stop,bandpass,low,high
 [h,w]=freqz(b,a,128);
 plot(abs(h));
 xlabel('frequency');
