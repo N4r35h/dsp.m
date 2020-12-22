e653c4d881234b871bf0470e7cb079b2fa980dfc
N4r35h <r.nar12345@gmail.com>
2020-12-22 13:36:11 +0530

    Create circular_conv.m

---
+++ circular_conv.m
@@ -0,0 +1,26 @@
+clc;
+clear all;
+close all;
+x=input('Enter the input 1 sequence: ');
+h=input('Enter the input 2 sequence: ');
+L=length(x);
+M=length(h);
+y=cconv(x,h); % “conv” inbuilt command used for convolution operation
+%------- To plot input 1 sequence-----------%
+subplot(3,1,1);
+stem(x);
+title('Input 1 sequence');
+xlabel('n');
+ylabel('x(n)');
+%------- To plot input 2 sequence-----------%
+subplot(3,1,2);
+stem(h);
+title('Input 2 sequence');
+xlabel('n');
+ylabel('h(n)');
+%------- To plot output sequence-----------%
+subplot(3,1,3);
+stem(y);
+title('Circular convolution sequence');
+xlabel('n');
+ylabel('y(n)');
